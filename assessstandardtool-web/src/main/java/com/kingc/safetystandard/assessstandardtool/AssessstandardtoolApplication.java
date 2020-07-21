15227a47974ac25296563d40d2f8663800ce772b
fuying56 <151253000@qq.com>
2020-07-21 08:41:06 +0800

    demo

--- assessstandardtool-web/src/main/java/com/kingc/safetystandard/assessstandardtool/AssessstandardtoolApplication.java
+++ assessstandardtool-web/src/main/java/com/kingc/safetystandard/assessstandardtool/AssessstandardtoolApplication.java
@@ -1,9 +1,12 @@
 package com.kingc.safetystandard.assessstandardtool;
 
+
+import tk.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.kingc.safetystandard.assessstandardtool.dao.**")
 public class AssessstandardtoolApplication {
 
     public static void main(String[] args){
